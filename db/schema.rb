cc89ec1b52e5fcc8ff43ce92f66e81179920d02d
Nelson Miranda <nmirandaghn@gmail.com>
2018-11-04 12:56:45 -0600

    Implement creating tickets for a project

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181101061301) do
+ActiveRecord::Schema.define(version: 20181104155955) do
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "tickets", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "project_id"
+    t.index ["project_id"], name: "index_tickets_on_project_id"
   end
 
 end
