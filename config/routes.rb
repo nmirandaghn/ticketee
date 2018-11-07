19dcf918e718a4f0c71f69dc642bc40507a79b06
Nelson Miranda <nmirandaghn@gmail.com>
2018-11-06 19:07:02 -0600

    Not finished yet signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "projects#index"
 
   resources :projects do
     resources :tickets
   end
+
+  resources :users
 end
