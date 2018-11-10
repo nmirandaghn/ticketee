f00aabde1ba85221a610e92d79752b53661423f8
Nelson Miranda <nmirandaghn@gmail.com>
2018-11-10 17:14:32 -0600

    Implemented sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,17 @@
 class SessionsController < ApplicationController
   def new
-    
   end
 
   def create
-    
+    user = User.find_by(name: params[:signin][:name])
+
+    if user && user.authenticate(params[:signin][:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Signed in succesfully"
+      redirect_to root_url
+    else
+      flash[:notice] = "Access denied " + user.name
+      render 'new'
+    end
   end
 end
