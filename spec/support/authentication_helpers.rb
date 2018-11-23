f98e168bc425f38bb73d340dfedd67760ede2d25
Nelson Miranda <nmirandaghn@gmail.com>
2018-11-22 19:07:54 -0600

    Unfinished creating permissions

--- spec/support/authentication_helpers.rb
+++ spec/support/authentication_helpers.rb
@@ -22,3 +22,14 @@ RSpec.configure do |c|
   c.include AuthHelpers, type: :controller
 end
 
+module AuthorizationHelpers
+  def define_permission!(user, action, thing)
+    Permission.create!(user: user,
+                      action: action,
+                      thing: thing)
+  end
+end
+
+RSpec.configure do |c|
+  c.include AuthorizationHelpers
+end
